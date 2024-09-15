0a3407f1ffb8c113a0ba83e2d0f322de46db8628
Legendlife0301 <legendlife0301@gmail.com>
2024-09-15 15:10:37 +0300

    Add files via upload

---
+++ modules/sorular.js
@@ -0,0 +1,7 @@
+module.exports = {
+    soru1: 'İsmini ve yaşını alabilir miyim?',
+    soru2: 'Daha önce hiç yetkili oldun mu?',
+    soru3: 'Günde kaç davet yapabilirsin?',
+    soru4: 'Günde kaç saat aktif olabilirsin?',
+    soru5: 'Bu sunucuya neler katacaksın?',
+  };
